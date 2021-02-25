b3d41fd1be9f47e5c76cd8ce589a5a295d96a866
Lucas <lucas.odonnelll@gmail.com>
2021-02-25 15:08:54 -0800

    created user model with devise and updated table and views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_25_225118) do
+ActiveRecord::Schema.define(version: 2021_02_25_225921) do
 
   create_table "events", force: :cascade do |t|
     t.integer "creator_id", null: false
@@ -22,6 +22,19 @@ ActiveRecord::Schema.define(version: 2021_02_25_225118) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["creator_id"], name: "index_events_on_creator_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "name", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "events", "creators"
